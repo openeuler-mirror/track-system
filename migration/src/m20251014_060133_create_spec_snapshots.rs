ef850e7481bce2a95d4d78233cba19c3065cc29b
Si Wang <wangs88@chinatelecom.cn>
2026-06-29 15:09:46 +0800

    module: introduce up, Migration and related helpers in migration/src/m20251014_060133_create_spec_snapshots.rs

---
+++ migration/src/m20251014_060133_create_spec_snapshots.rs
@@ -0,0 +1,39 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(SpecSnapshots::Table)
+                    .if_not_exists()
+                    .col(pk_auto(SpecSnapshots::Id))
+                    .col(integer(SpecSnapshots::CommitRecordId))
+                    .col(string(SpecSnapshots::SpecFilename))
+                    .col(string(SpecSnapshots::Name))
+                    .col(string(SpecSnapshots::Version))
+                    .col(string(SpecSnapshots::Release))
+                    .col(text_null(SpecSnapshots::Summary))
+                    .col(string_null(SpecSnapshots::License))
+                    .col(text(SpecSnapshots::Sources))
+                    .col(text(SpecSnapshots::Patches))
+                    .col(text_null(SpecSnapshots::LatestChangelogEntry))
+                    .col(text(SpecSnapshots::FullContent))
+                    .col(string(SpecSnapshots::ContentHash))
+                    .col(text(SpecSnapshots::DownloadUrl))
+                    .col(timestamp(SpecSnapshots::FetchedAt))
