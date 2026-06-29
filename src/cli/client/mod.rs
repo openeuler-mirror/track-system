0eaba796d115b0cdfbc7b7ecc61618f4db77b32b
Si Wang <wangs88@chinatelecom.cn>
2026-06-29 15:10:16 +0800

    module: introduce config, error and related helpers in src/cli/client/mod.rs

---
+++ src/cli/client/mod.rs
@@ -0,0 +1,22 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+//! API 客户端模块
+//!
+//! 提供与 track-server HTTP API 通信的客户端实现
+
+pub mod config;
+pub mod error;
+pub mod http;
+
+pub use config::ClientConfig;
+pub use error::{ApiError, ApiResult};
+pub use http::ApiClient;
