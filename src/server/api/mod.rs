a48b8c19cac719cdaf3ff1581bbf078139b813ca
Si Wang <wangs88@chinatelecom.cn>
2026-07-01 10:04:56 +0800

    module: introduce response in src/server/api/mod.rs
    修改内容:
    - 在 src/server/api/mod.rs 中引入 response。
    - 补充模块符号：response。
    
    实现功能:
    - 建立 response 的模块边界，为后续业务实现和跨模块调用提供类型与接口基础。

---
+++ src/server/api/mod.rs
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+//! API 模块
+//!
+//! 提供统一的 RESTful API 接口
+
+pub mod response;
+
+pub use response::{
+    ApiResponse, ComponentStatus, ErrorResponse, HealthComponents, HealthResponse,
+    PaginatedResponse,
+};
