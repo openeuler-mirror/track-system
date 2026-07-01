b3805b3603ae2813e3e0245956e822bea3789156
Si Wang <wangs88@chinatelecom.cn>
2026-07-01 10:07:02 +0800

    detail: implement list_packages in src/server/handlers/package.rs
    修改内容:
    - 在 src/server/handlers/package.rs 中实现 list_packages。
    - 落入处理入口/辅助函数：list_packages。
    
    实现功能:
    - 交付 list_packages 对应的运行时行为，使相关请求、调度、采集或数据处理路径具备实际执行能力。

---
+++ src/server/handlers/package.rs
@@ -0,0 +1,40 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    Json,
+};
+use chrono::Utc;
+use sea_orm::*;
+
+use crate::{
+    entities::{packages, prelude::*},
+    server::{
+        dto::{
+            CreatePackageRequest, PackageResponse, PackageWithTrackingResponse, TrackingResponse,
+            UpdatePackageRequest,
+        },
+        error::{ApiError, ApiResult},
+        state::AppState,
+    },
+};
+
+/// 列出所有软件包
+pub async fn list_packages(State(state): State<AppState>) -> ApiResult<Json<Vec<PackageResponse>>> {
+    let packages = Packages::find().all(state.db.as_ref()).await?;
+
+    let responses: Vec<PackageResponse> = packages.into_iter().map(Into::into).collect();
+
+    Ok(Json(responses))
+}
+
