0604d59c6069a57c390d0e91c6e614164cc12dcb
Si Wang <wangs88@chinatelecom.cn>
2026-07-01 10:07:18 +0800

    detail: implement backport_routes in src/server/routes/backport.rs
    修改内容:
    - 在 src/server/routes/backport.rs 中实现 backport_routes。
    - 落入处理入口/辅助函数：backport_routes。
    
    实现功能:
    - 交付 backport_routes 对应的运行时行为，使相关请求、调度、采集或数据处理路径具备实际执行能力。

---
+++ src/server/routes/backport.rs
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+use axum::{routing::get, Router};
+
+use crate::server::{handlers::backport, state::AppState};
+
+pub fn backport_routes() -> Router<AppState> {
+    Router::new().route(
+        "/packages/:package_id/backports",
+        get(backport::list_backport_candidates_handler),
+    )
+}
