c389dd4acee6d46e215bee5c96da1771c056bb34
Si Wang <wangs88@chinatelecom.cn>
2026-07-01 10:04:01 +0800

    module: introduce from, GitHubRepository and related helpers in src/collectors/github/models.rs
    修改内容:
    - 在 src/collectors/github/models.rs 中引入 from, GitHubRepository and related helpers。
    - 补充模块符号：from, GitHubRepository, From<GitHubRepository>。
    
    实现功能:
    - 建立 from, GitHubRepository and related helpers 的模块边界，为后续业务实现和跨模块调用提供类型与接口基础。

---
+++ src/collectors/github/models.rs
@@ -0,0 +1,38 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+use crate::collectors::traits::{Branch, Commit, CommitStats, FileContent, Repository};
+
+#[derive(Debug, Deserialize)]
+pub struct GitHubRepository {
+    pub id: i64,
+    pub name: String,
+    pub full_name: String,
+    pub description: Option<String>,
+    pub html_url: String,
+    #[serde(default)]
+    pub default_branch: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl From<GitHubRepository> for Repository {
+    fn from(repo: GitHubRepository) -> Self {
+        Repository {
+            id: repo.id,
+            name: repo.name,
+            full_name: repo.full_name,
+            description: repo.description,
+            html_url: repo.html_url,
+            default_branch: if repo.default_branch.is_empty() {
