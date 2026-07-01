f9818e191c4b4da52d9cc560e3c3227b46d7350c
Si Wang <wangs88@chinatelecom.cn>
2026-07-01 10:04:32 +0800

    module: introduce Model in src/entities/compare_reports.rs
    修改内容:
    - 在 src/entities/compare_reports.rs 中引入 Model。
    - 补充模块符号：Model。
    
    实现功能:
    - 建立 Model 的模块边界，为后续业务实现和跨模块调用提供类型与接口基础。

---
+++ src/entities/compare_reports.rs
@@ -0,0 +1,25 @@
+/*
+ * Copyright(c) 2024-2026 China Telecom Cloud Technologies Co., Ltd. All rights
+ * reserved. ctscat is licensed under Mulan PSL v2. You can use this software
+ * according to the terms and conditions of the Mulan PSL V2. You may obtain a
+ * copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  See the Mulan PSL v2 for
+ * more details.
+ */
+
+use sea_orm::{entity::prelude::*, JsonValue};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+#[sea_orm(table_name = "compare_reports")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub tracking_id: i32,
+    pub generated_at: DateTimeUtc,
+    #[sea_orm(column_type = "JsonBinary", nullable)]
+    pub l2_vs_l1_diff: Option<JsonValue>,
+    #[sea_orm(column_type = "JsonBinary", nullable)]
+    pub l1_vs_l0_diff: Option<JsonValue>,
